926dfdd4efd67bceb2f8b1980f23acca7e21df2a
John Almazan <johnl.almazan@gmail.com>
2025-03-01 12:25:34 +0000

    add home page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    """
+    View function for the home page of the site.
+    """
+    return render(request, 'core/home.html')
